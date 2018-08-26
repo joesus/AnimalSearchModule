6c5837bbf4c686f9c0c0b9e70c511c4a021bdb80
joesus <joesusnick@gmail.com>
2018-08-26 15:49:25 -0600

    Creating project

---
+++ AnimalSearchModule/AnimalSearchModule/AnimalSearchModule.h
@@ -0,0 +1,19 @@
+//
+//  AnimalSearchModule.h
+//  AnimalSearchModule
+//
+//  Created by Joe Susnick on 8/26/18.
+//  Copyright © 2018 Joe Susnick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimalSearchModule.
+FOUNDATION_EXPORT double AnimalSearchModuleVersionNumber;
+
+//! Project version string for AnimalSearchModule.
+FOUNDATION_EXPORT const unsigned char AnimalSearchModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimalSearchModule/PublicHeader.h>
+
+
